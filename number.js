e95b7708336856fcf3a2a0a9ba991dd4fca5431a
Xavier S Howell <xmanhowell9@gmail.com>
2024-03-29 17:02:13 -0500

    scaleabsds

--- number.js
+++ number.js
@@ -24,7 +24,8 @@ const userStates = {}; // This object will map session IDs to game states
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
+app.use(express.cookieParser());
+ app.use(express.session());
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get("*", (req,res)=> {
